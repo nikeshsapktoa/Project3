bcc148c900458d2f25bf722c67c6f00f8f8bbb08
CobyMikel <cobymikel@gmail.com>
2025-03-03 10:25:21 -0600

    Add files via upload

---
+++ Falling_platform.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+@export var fall_delay: float = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		await get_tree().create_timer(fall_delay).timeout
+		print("player entered")
+		gravity_scale = 1
