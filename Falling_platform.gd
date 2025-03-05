af4200fde8e3bfe3bfe03e058353504e0696cd9d
CobyMikel <cobymikel@gmail.com>
2025-03-05 10:13:29 -0600

    Update Falling_platform.gd

--- Falling_platform.gd
+++ Falling_platform.gd
@@ -3,9 +3,9 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	gravity_scale=0
+	freeze=true
+	$Area2D.body_entered.connect(_on_area_2d_body_entered)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -15,4 +15,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		await get_tree().create_timer(fall_delay).timeout
 		print("player entered")
+		freeze=false
 		gravity_scale = 1
